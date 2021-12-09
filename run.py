06f7513a07de89f1a42a468d897f3d439f22b985
jamesnathanan <artofgems@live.com>
2021-12-09 21:07:06 +0700

    insert records

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -24,6 +25,33 @@ class Publication(db.Model):
 	
 	def __repr__(self):
 		return 'The ID is {} , The Name is {} '.format(self.id, self.name)
+
+class Book(db.Model):
+	__tabel_name = 'book'
+
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(500), nullable=False, index=True)
+	author = db.Column(db.String(350))
+	avg_rating = db.Column(db.Float)
+	format = db.Column(db.String(50))
+	image = db.Column(db.String(100), unique=True)
+	num_pages = db.Column(db.Integer)
+	pub_date = db.Column(db.DateTime, default=datetime.utcnow())
+
+	# Relationship
+	pub_id = db.Column(db.Integer, db.ForeignKey('publication.id'))
+	
+	def __init__(self, title, author, avg_rating, book_format, image, num_pages,  pub_id):
+		self.title = title
+		self.author = author
+		self.avg_rating = avg_rating
+		self.format = book_format
+		self.image = image
+		self.num_pages = num_pages
+		self.pub_id = pub_id
+
+	def __repr__(self):
+		return '{} by {}'.format(self.title, self.author)
 
 @app.route('/index')
 @app.route('/')
